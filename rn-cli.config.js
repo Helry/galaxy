f50738bb194282a2034cb309f366d05794b2a468
杨俊宁 <1003719811@qq.com>
2018-10-23 13:43:51 +0800

    升级版本到 0.57.3

--- rn-cli.config.js
+++ rn-cli.config.js
@@ -1,9 +1,10 @@
 // @name Make ReactNative Great Again
 // @description Allows you to enable support for JSX files, and `.mjs` files which is the new node standard
 // @source http://www.fallingcanbedeadly.com/posts/enabling-react-native-jsx-extension
-// @issues https://github.com/airbnb/javascript/issues/982
-// @issues https://github.com/facebook/metro/issues/248
+// @source https://github.com/airbnb/javascript/issues/982
 // @note One caveat, The `index.js` file in the root of your project has to be `.js`.
 module.exports = {
-  getSourceExts: () => ['jsx', 'mjs'],
+  resolver: {
+    sourceExts: ['js', 'json', 'ts', 'tsx', 'jsx'],
+  },
 }
